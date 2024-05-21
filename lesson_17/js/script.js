cf4a46c53e64023bf115460959d78851cddb1d94
Yershov Artem <fortestsante@gmail.com>
2024-05-21 08:22:28 +0300

    lesson 17 addede prepearing for check

---
+++ lesson_17/js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	// .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
+	if (targetElement.closest('.button-burger')) {
+		document.documentElement.classList.remove('search-open');
+		document.documentElement.classList.remove('catalog-open');
+		document.documentElement.classList.toggle('burger-open');
+		e.preventDefault();
+		console.log(document);
+	}
+})
+
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	// .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
+	if (targetElement.closest('.buy-icons__link-icon--none') || targetElement.closest('.buy-icons__link-icon--cross')) {
+		document.documentElement.classList.remove('burger-open');
+		document.documentElement.classList.remove('catalog-open');
+		document.documentElement.classList.toggle('search-open');
+		e.preventDefault();
+		console.log(document);
+	}
+})
+
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	// .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
+	if (targetElement.closest('.button--catalog') ) {
+		document.documentElement.classList.remove('burger-open');
+		document.documentElement.classList.remove('search-open');
+		document.documentElement.classList.toggle('catalog-open');
+		e.preventDefault();
+		console.log(document);
+	}
+})
